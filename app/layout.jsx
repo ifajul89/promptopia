ffa5fdda791d9edbafa4a04802d11f84f4c5f083
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-08-12 03:25:04 +0600

    added basic background and layout

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "../styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover and Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+
+          <main className="app">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
